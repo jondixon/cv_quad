eec8f2f4359322397ee5706f641ba6365d0b1619
Jon Dixon <jon.d.dixon@gmail.com>
2015-09-02 23:40:26 -0600

    Converted existing code to reside in a class and added shutdown function L2+SELECT+CIRCLE #2

--- get_controller_input/controller_test.py
+++ get_controller_input/controller_test.py
@@ -3,49 +3,56 @@ import sys
 import time
 import StringIO
 
-def getButtonsPressed():
-    buttonsPressed = [dualshock.get_axis(0),
-                      -dualshock.get_axis(1),
-                      dualshock.get_axis(2),
-                      -dualshock.get_axis(5),
-                      dualshock.get_button(0),
-                      dualshock.get_button(1),
-                      dualshock.get_button(2),
-                      dualshock.get_button(3),
-                      dualshock.get_button(4),
-                      dualshock.get_button(5),
-                      dualshock.get_button(6),
-                      dualshock.get_button(7),
-                      dualshock.get_button(8),
-                      dualshock.get_button(9)]
-    return buttonsPressed
+class controller:
+    def __init__(self):
+        self.state = []
+        pygame.init()
 
-pygame.init()
+        joystick_count = pygame.joystick.get_count()
+        print "Joystick count", joystick_count
+        if joystick_count == 0:
+            print "No Joystick Connected"
+            sys.exit()
 
-joystick_count = pygame.joystick.get_count()
-print "Joystick count", joystick_count
-if joystick_count == 0:
-    print "No Joystick Connected"
-    sys.exit()
+        self.dualshock = pygame.joystick.Joystick(0)
+        self.dualshock.init()
 
-dualshock = pygame.joystick.Joystick(0)
-dualshock.init()
+        if not self.dualshock.get_init():
+            print "Could not initialize joystick."
+            sys.exit()
 
-if not dualshock.get_init():
-    print "Could not initialize joystick."
-    sys.exit()
 
-while 1:
-    #delay to simulate a "frame"
-    time.sleep(.05)
+    def readState(self):
+        #delay to simulate a "frame"
+        time.sleep(.05)
+        #use this to clear out the event queue to update the joystick table
+        pygame.event.pump()
+        self.state = [self.dualshock.get_axis(0),
+                      -self.dualshock.get_axis(1),
+                      self.dualshock.get_axis(2),
+                      -self.dualshock.get_axis(5),
+                      self.dualshock.get_button(0),
+                      self.dualshock.get_button(1),
+                      self.dualshock.get_button(2),
+                      self.dualshock.get_button(3),
+                      self.dualshock.get_button(4),
+                      self.dualshock.get_button(5),
+                      self.dualshock.get_button(6),
+                      self.dualshock.get_button(7),
+                      self.dualshock.get_button(8),
+                      self.dualshock.get_button(9)]
 
-    #use this to clear out the event queue to update the joystick table
-    pygame.event.pump()
+        if self.state[12] and self.state[10] and self.state[6]:
+            sys.exit()
 
-    #Get all axes and buttons pressed
-    buttonsPressed = getButtonsPressed()
+    def getState(self):
+        return self.state
 
-    print buttonsPressed
+if __name__ == "__main__":
+    cState = controller()
+    while 1:
 
-    if dualshock.get_button(12):
-        sys.exit()
+        #Get all axes and buttons pressed
+        cState.readState()
+
+        print cState.getState() 

daf4a2a997a79c93ffca1e6408705d16d4610a1f
Jon Dixon <jon.d.dixon@gmail.com>
2015-09-10 16:35:49 -0600

    fixing how user input is read. #5

--- pwm_control/motor_test.py
+++ pwm_control/motor_test.py
@@ -1,9 +1,10 @@
 import motorPWM
+import sys
 
 print "About to initialize motor"
 
 motor1 = motorPWM.motor(27)
 
 while (1):
-    motor1Percent = raw_input("Enter percent thrust for motor 1: ")
+    motor1Percent = input("Enter percent thrust for motor 1: ")
     motor1.setRotor(motor1Percent)

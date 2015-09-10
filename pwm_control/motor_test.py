61c261b0fad0e0883a6fca3f6bd0fff207cc231b
Jon Dixon <jon.d.dixon@gmail.com>
2015-09-10 16:33:27 -0600

    fixed motor_test.py. forgot to put the prefix in front of the class. #5

--- pwm_control/motor_test.py
+++ pwm_control/motor_test.py
@@ -2,7 +2,7 @@ import motorPWM
 
 print "About to initialize motor"
 
-motor1 = motor(27)
+motor1 = motorPWM.motor(27)
 
 while (1):
     motor1Percent = raw_input("Enter percent thrust for motor 1: ")

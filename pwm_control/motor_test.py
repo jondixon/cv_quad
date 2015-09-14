d3552b7cf2b21ef5d678fdd4e2a17af2ac079b2f
Jon Dixon <jon.d.dixon@gmail.com>
2015-09-14 21:17:30 +0000

    initial test of all four rotors. #5

--- pwm_control/motor_test.py
+++ pwm_control/motor_test.py
@@ -3,7 +3,15 @@ import sys
 
 print "About to initialize motor"
 
-motor1 = motorPWM.motor(27)
+
+motor1 = motorPWM.motor(05)
+motor2 = motorPWM.motor(06)
+motor3 = motorPWM.motor(13)
+motor4 = motorPWM.motor(19)
 
 while (1):
-    motor1.setRotor(input("Enter percent thrust for motor 1: "))
+    thrust = input("Enter percent thrust: ")
+    motor1.setRotor(thrust)
+    motor2.setRotor(thrust)
+    motor3.setRotor(thrust)
+    motor4.setRotor(thrust)
